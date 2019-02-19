e9788af7e0374a9f3ba5b6bf60ac10a0369a9b1b
Remi- <remi-castel@hotmail.fr>
2019-02-19 16:08:59 +0100

    Blocage: n'arrive pas à récupérer le nom des variables JCheckBox en dehors de la boucle for

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,25 +1,5 @@
 
-import com.amazonaws.regions.Region;
-import com.amazonaws.services.rekognition.AmazonRekognition;
-import com.amazonaws.services.rekognition.AmazonRekognitionClientBuilder;
-import com.amazonaws.services.rekognition.model.Image;
-import com.amazonaws.services.rekognition.model.BoundingBox;
-import com.amazonaws.services.rekognition.model.CompareFacesMatch;
-import com.amazonaws.services.rekognition.model.CompareFacesRequest;
-import com.amazonaws.services.rekognition.model.CompareFacesResult;
-import com.amazonaws.services.rekognition.model.ComparedFace;
-import java.util.List;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStream;
-import java.nio.ByteBuffer;
-import com.amazonaws.util.IOUtils;
-import javafx.application.Application;
-import javafx.stage.Stage;
-import org.bytedeco.javacv.CanvasFrame;
-import model.dal.PersistenceManager;
-
-import javax.swing.*;
+import view.interface_emprunt;
 
 public class Main {
 
--- src/main/java/view/interface_emprunt.java
+++ src/main/java/view/interface_emprunt.java
@@ -1,3 +1,5 @@
+package view;
+
 import com.amazonaws.event.DeliveryMode;
 import com.google.api.client.json.Json;
 import model.Borrow;
@@ -15,41 +17,45 @@ import java.util.Date;
 
 public class interface_emprunt extends JFrame implements ItemListener {
 
-
+    static JFrame f;
     public interface_emprunt() throws SQLException{
 
-        super("JavaIdentifier");
-
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f = new JFrame("JavaIdentifier");
+        setDefaultCloseOperation(f.EXIT_ON_CLOSE);
         Toolkit tk = Toolkit.getDefaultToolkit();
         Dimension d = tk.getScreenSize();
         int hauteurEcran = d.height;
         int largeurEcran = d.width;
-        setSize(largeurEcran/2, hauteurEcran/2);
-        setLocation(largeurEcran/4, hauteurEcran/4);
         Image img = tk.getImage("C:\\Users\\Remi-\\IdeaProjects\\API\\Images\\logo.jpg");
         setIconImage(img);
-        JButton b1 = new JButton("Valider");
-        add(b1,BorderLayout.WEST);
+
         EquipmentDAO Equipements = new EquipmentDAO();
         Integer nb= Equipements.getEquipments().size();
-        setLayout(new GridLayout(nb, 1));
 
         JCheckBox[] tableau = new JCheckBox[nb];
-
-
+        JPanel container = new JPanel();
+        JPanel p = new JPanel();
+        JPanel b = new JPanel();
         for(int i=0;i<nb;i++)
         {
-
             String nom = Equipements.getEquipments().get(i).getName();
             tableau[i]= new JCheckBox((String)Equipements.getEquipments().get(i).getName(), false);
-            add(new Checkbox(nom));
+            p.add(new Checkbox(nom));
             tableau[i].setMnemonic(KeyEvent.VK_C);
             tableau[i].addItemListener(this);
             Integer Id = Equipements.getEquipments().get(i).getId();
         }
-            setVisible(true);
 
+
+        JButton b1 = new JButton("Valider");
+
+        f.setSize(hauteurEcran,largeurEcran );
+        container.setLayout(new BoxLayout(container,BoxLayout.PAGE_AXIS));
+        b.add(b1);
+        container.add(p);
+        container.add(b);
+        f.add(container);
+        f.show();
 
        b1.addActionListener(new ActionListener()
         {
@@ -59,6 +65,7 @@ public class interface_emprunt extends JFrame implements ItemListener {
                 BorrowDAO BorrowDao = new BorrowDAO();
                 System.out.println(tableau[1].getName());
                 for (int j = 0; j < nb; j++) {
+
 
                     if (tableau[j].isSelected() == true) {
                         Borrow Emprunt = null;
